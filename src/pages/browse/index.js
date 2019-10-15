c9a4cf256c1e5928e659f1ae97f38929aadc8747
Guilherme Di Paula <guilepaul@gmail.com>
2019-10-14 23:37:49 -0300

    pages - browse e playlist

--- src/pages/browse/index.js
+++ src/pages/browse/index.js
@@ -1,10 +1,62 @@
+/* eslint-disable no-trailing-spaces */
+/* eslint-disable indent */
+/* eslint-disable comma-dangle */
 import React from 'react';
 
-import { Container, Title } from './styles';
+import {
+ Container, Title, List, Playlist 
+} from './styles';
 
 const Browse = () => (
   <Container>
     <Title>Navegar</Title>
+
+    <List>
+      <Playlist to="/playlists/1">
+        <img
+          src="https://99designs-blog.imgix.net/blog/wp-content/uploads/2017/12/Stargroves-album-cover.png?auto=format&q=60&fit=max&w=930"
+          alt="Playlist"
+        />
+        <strong>Rock dos bons</strong>
+        <p>
+          Relaxe enquanto você programa ouvindo apenas as melhores do rock
+          mundial!
+        </p>
+      </Playlist>
+      <Playlist to="/playlists/1">
+        <img
+          src="https://99designs-blog.imgix.net/blog/wp-content/uploads/2017/12/Stargroves-album-cover.png?auto=format&q=60&fit=max&w=930"
+          alt="Playlist"
+        />
+        <strong>Rock dos bons</strong>
+        <p>
+          Relaxe enquanto você programa ouvindo apenas as melhores do rock
+          mundial!
+        </p>
+      </Playlist>
+      <Playlist to="/playlists/1">
+        <img
+          src="https://99designs-blog.imgix.net/blog/wp-content/uploads/2017/12/Stargroves-album-cover.png?auto=format&q=60&fit=max&w=930"
+          alt="Playlist"
+        />
+        <strong>Rock dos bons</strong>
+        <p>
+          Relaxe enquanto você programa ouvindo apenas as melhores do rock
+          mundial!
+        </p>
+      </Playlist>
+      <Playlist to="/playlists/1">
+        <img
+          src="https://99designs-blog.imgix.net/blog/wp-content/uploads/2017/12/Stargroves-album-cover.png?auto=format&q=60&fit=max&w=930"
+          alt="Playlist"
+        />
+        <strong>Rock dos bons</strong>
+        <p>
+          Relaxe enquanto você programa ouvindo apenas as melhores do rock
+          mundial!
+        </p>
+      </Playlist>
+    </List>
   </Container>
 );
 
---
+++ src/pages/playlist/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+// import { Container } from './styles';
+
+const Playlist = () => <h1>Hello</h1>;
+
+export default Playlist;
